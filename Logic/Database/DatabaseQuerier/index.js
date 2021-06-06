7ebf8b48b5c4bf93d9a427bc79d35ad461dcb363
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-06 13:06:44 -0700

    This works much better

--- Logic/Database/DatabaseQuerier/index.js
+++ Logic/Database/DatabaseQuerier/index.js
@@ -1,58 +1,22 @@
 const mysql = require('mysql2/promise')
 
-class DatabaseQuerier {
+class DatabaseQuerier { //TODO: Refactor this to be a module with functions instead of a class. No need to be a class now that we're connecting to db on each query
     endpoint = process.env.DB_ENDPOINT;
     username = process.env.DB_USER;
     password = process.env.DB_PASSWORD;
     databaseName = process.env.DB_NAME;
-    connection
-
 
     constructor() {
-        this.connectToDatabase()
     }
 
-    connectToDatabasePromise(){
+    connectToDatabase(){
         return mysql.createConnection({
             host: this.endpoint,
             user: this.username,
             password: this.password,
             database: this.databaseName
         })
-    }
-
-    connectToDatabase() {
-        this.connectToDatabasePromise().then((newConnection) => {
-            this.connection = newConnection;            
-            console.log("DatabaseQuerier. Connection to db created");
-        }).catch((error) => {
-            console.log("ERROR - DatabaseQuerier. Failed to connect to db: ", error);
-            this.destroyAndReconnect();
-        });                
-    }
-
-    destroyAndReconnect = async ()=>{
-        try{
-            if(this.connection) await this.connection.destroy();
-            setTimeout(()=>{
-                console.log("DatabaseQuerier. Destroyed connection. Attempting to reconnect in 2 seconds");
-                this.connectToDatabase();
-            }, 2000)            
-        }
-        catch(error){
-            console.log("ERROR -- DatabaseQuerier: ", error);
-        }                                        
-    }
-
-    destroyAndReconnectSingleAttempt = async()=>{
-        try{
-            if(this.connection) await this.connection.destroy();
-            this.connection = await this.connectToDatabasePromise();
-        }
-        catch(error){
-            console.log("ERROR -- DatabaseQuerier::destroyAndReconnectSingleAttempt: ", error);
-        } 
-    }
+    }    
 
     /**
      * Returns response promise
@@ -70,25 +34,16 @@ class DatabaseQuerier {
                 throw 'null bind param';
             }
         }
+
         console.log("DatabaseQuerier::executeQuery. Executing");
-        try{
-            return await this.connection.execute(query, bindParams);
-        }
-        catch(error){
-            console.log("ERROR -- DatabaseQuerier::executeQuery: ", error);
-            if(error.code === 'ETIMEDOUT' && !retrying){
-                await this.destroyAndReconnectSingleAttempt();
-                return await this.executeQuery(query, bindParams, true);
-            }           
-            else{
-                throw error;
-            }
-        }
+        const databaseConnection = await this.connectToDatabase();
+        return await databaseConnection.execute(query, bindParams);       
     }
 
-    _runTestQuery() {
+    async _runTestQuery() {
+        const databaseConnection = await this.connectToDatabase();
         const testQuery = `SELECT * FROM users`;
-        this.connection.execute(testQuery).then((response) => {
+        databaseConnection.execute(testQuery).then((response) => {
             console.log(response[0][0]);
             console.log((response[0][0].id));
         })
