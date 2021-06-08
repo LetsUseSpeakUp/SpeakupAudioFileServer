a94a1a239e233d27c3ec1ab3a0b81733d91ecc93
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-08 12:14:44 -0700

    Deleted getApprovalInfo because we shouldn't be using that anymore.

--- Routes/Convos/index.js
+++ Routes/Convos/index.js
@@ -5,6 +5,5 @@ router.use('/upload', require('./Upload'));
 router.use('/getmetadata', require('./GetMetaData'));
 router.use('/setapproval', require('./SetApproval'));
 router.use('/retrieve', require('./Retrieve'));
-router.use('/getapprovalinfo', require('./GetApprovalInfo'));
 
 module.exports = router;
