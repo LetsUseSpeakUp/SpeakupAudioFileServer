de6bbf3e8fa6f08ce12a37fc1bcb621cbe18a558
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-10 15:44:28 -0700

    Got it loading the audio in iMessage.

--- Logic/Encryption/index.js
+++ Logic/Encryption/index.js
@@ -4,16 +4,29 @@ const IV = process.env.QUERY_INIT_VEC; //16 char
 
 
 const getEncryptedString = (stringToEncrypt) => {
-    const cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
-    let encrypted = cipher.update(stringToEncrypt, 'utf8', 'base64');
-    encrypted += cipher.final('base64');
-    return encrypted
+    try{
+        const cipher = crypto.createCipheriv('aes-256-cbc', ENC_KEY, IV);
+        let encrypted = cipher.update(stringToEncrypt, 'utf8', 'base64');
+        encrypted += cipher.final('base64');
+        return encrypted
+    }
+    catch(error){
+        console.log("ERROR -- Encryption::getEncryptedString: ", error);
+        return '';
+    }
+    
 }
 
 const getDecryptedString = (stringToDecrypt) => {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
-    const decrypted = decipher.update(stringToDecrypt, 'base64', 'utf8');
-    return (decrypted + decipher.final('utf8'));
+    try{
+        const decipher = crypto.createDecipheriv('aes-256-cbc', ENC_KEY, IV);
+        const decrypted = decipher.update(stringToDecrypt, 'base64', 'utf8');
+        return (decrypted + decipher.final('utf8'));
+    }
+    catch(error){
+        console.log("ERROR -- Encryption::getDecryptedString");
+        return '';
+    }    
 }
 
 exports.getEncryptedString = getEncryptedString;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
+const fs = require('fs');
 
 global.reqlib = require('app-root-path').require;
 
@@ -29,9 +30,19 @@ app.listen(port,  ()=>{
 app.use('/backend', require('./Routes'));
 
 app.use(express.static(path.join(__dirname, './react_frontend/build')));
-app.get('/playsnippet', (req, res)=>{ //TODO: Inject opengraph tags
+app.get('/playsnippet', (req, res)=>{
     console.log("Index::query params: ", req.url);
-    res.sendFile(path.resolve(__dirname, './react_frontend/build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, './react_frontend/build', 'index.html'));
+    const filePath = path.resolve(__dirname, './react_frontend/build', 'index.html');
+    fs.readFile(filePath, 'utf8', function(err, data){
+        if(err){
+            return console.log("ERROR -- main index serving /playsnippet: ", err);
+        }
+        data = data.replace(/\$OG_TITLE/g, 'SpeakUp Snippet');
+        data = data.replace(/\$OG_DESCRIPTION/g, 'Listen to this snippet from SpeakUp');
+        const result = data.replace(/\$OG_AUDIO_LINK/g, 'http://techtechandtechcom.ipage.com/rapgame/SJaFmWwSO-wont_stop_til_I_win,_Im_destined_to_win.mp4'); //TODO
+        return res.send(result);
+    })    
 })
 
 app.get('*', (req, res)=>{ //This is so we get a decent looking 404    
