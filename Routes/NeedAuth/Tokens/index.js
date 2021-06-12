3e21087fe283671c2aaa97ad12840b184df9da3a
Faraz Abidi <farazabidi@farazs-air-3.lan>
2021-06-12 08:39:58 -0700

    Implemented rtm token generation.

--- Routes/NeedAuth/Tokens/index.js
+++ Routes/NeedAuth/Tokens/index.js
@@ -1,49 +1,69 @@
 const router = require('express').Router();
-const {RtcTokenBuilder, RtcRole} = require('agora-access-token')
-const {addStartedConvo} = require('../../../Logic/Database/ConvosDatabase')
+const { RtcTokenBuilder, RtcRole, RtmTokenBuilder, RtmRole } = require('agora-access-token')
+const { addStartedConvo } = require('../../../Logic/Database/ConvosDatabase')
 
 const APP_ID = process.env.APP_ID;
 const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
 
-router.post('/getchanneltoken', async(req, res)=>{        
+router.post('/getchanneltoken', async (req, res) => { //TODO: Make this a GET
     const channelName = req.body.channel;
     const isInitiator = (req.body.isInitiator.toLowerCase() === 'true');
-    const phone_number = req.user['https://backend.letsusespeakup.com/token/usermetadata/phone_number'] 
+    const phone_number = req.user['https://backend.letsusespeakup.com/token/usermetadata/phone_number']
         || req.user['https://backend.letsusespeakup.com/token/usermetadata/metadata'].phone_number;
 
     const first_name = req.user['https://backend.letsusespeakup.com/token/usermetadata/metadata'].first_name;
-    const last_name = req.user['https://backend.letsusespeakup.com/token/usermetadata/metadata'].last_name;    
-    
-    if(!channelName){
+    const last_name = req.user['https://backend.letsusespeakup.com/token/usermetadata/metadata'].last_name;
+
+    if (!channelName) {
         return res.status(500).send({
             message: 'no channel name'
         });
     }
-    if(isInitiator == null){
+    if (isInitiator == null) {
         return res.status(500).send({
             message: 'is initiator not set'
         });
     }
-    if(!phone_number){
+    if (!phone_number) {
         return res.status(500).send({
             message: 'no phone number'
         });
-    }    
+    }
     //TODO: Verify that user phone number is part of channel name
     console.log("/Tokens/getchannelToken. is initiator: ", isInitiator);
     const addConvoResponse = await addStartedConvo(channelName, isInitiator, phone_number, first_name, last_name);
-    if(!addConvoResponse.success) return res.status(500).send({
+    if (!addConvoResponse.success) return res.status(500).send({
         message: addConvoResponse.errorMessage
     });
 
     let uid = req.body.uid;
-    if(!uid) uid = 0;
+    if (!uid) uid = 0;
 
     const currentTimestamp = Math.floor(Date.now() / 1000)
-    const timeToLive = 30*60;
+    const timeToLive = 30 * 60;
     const privilegeExpiredTs = currentTimestamp + timeToLive;
 
     const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE, channelName, uid, RtcRole.PUBLISHER, privilegeExpiredTs);
+    return res.status(200).send({
+        token: token
+    });
+})
+
+router.get('/getrtmtoken', (req, res) => {
+    const phone_number = req.user['https://backend.letsusespeakup.com/token/usermetadata/phone_number']
+        || req.user['https://backend.letsusespeakup.com/token/usermetadata/metadata'].phone_number;
+
+    if (!phone_number) {
+        return res.status(500).send({
+            message: 'no phone number'
+        });
+    }
+
+    const currentTimestamp = Math.floor(Date.now() / 1000)
+    const timeToLive = 30 * 60 * 24;
+    const privilegeExpiredTs = currentTimestamp + timeToLive;
+
+    const token = RtmTokenBuilder.buildToken(APP_ID, APP_CERTIFICATE, phone_number, RtmRole.Rtm_User, privilegeExpiredTs);
     return res.status(200).send({
         token: token
     });
